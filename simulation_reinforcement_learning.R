7094dc1ab96b11795460037d2a7259dbaef2778c
Alexia Gaudeul <a.gaudeul@gmail.com>
2016-09-01 15:52:37 +0200

    table of results

--- simulation_reinforcement_learning.R
+++ simulation_reinforcement_learning.R
@@ -37,6 +37,7 @@ library(distr)
 v<-5
 e<-1
 vmu<-c(0,0.3,0.7,1)
+#vmu<-c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
 vlambda<-c(0,0.3,100)
 
 # LOOPING
@@ -61,15 +62,17 @@ for(mu in unique(vmu)){
 # initialize storage of results
   gA<-pricesA
   gBC<-pricesBC
-  exp_profitA<-0
-  exp_profitBC<-0
+  profitA<-0
+  profitBC<-0
+  formatA_BC<-0
+  
 
-# 30 iterations
+# rounds of learning iterations
   i<-1
-  N<-10000
-  while(i<=30){
+  N<-100000
+  while(i<=100){
     set.seed(1)                      # for reproduceable example
-    print(i)
+    #print(i)
 
     pA<-c(rdistA(N))
     pA<-c(pA,pricesA,pricesA) # guarantee full support
@@ -98,12 +101,15 @@ for(mu in unique(vmu)){
     p_adjustBC<-mean_profitBC/sum(mean_profitBC)
 
     # giving results as they go
-    print(sum(p_adjustA*mean_profitA)) # expected profit A
-    print(sum(p_adjustBC*mean_profitBC))
-    print(sum(p_adjustBC[51:100])) # percentage CS
+    profit_A<-sum(p_adjustA*mean_profitA) # expected profit A
+    profit_BC<-sum(p_adjustBC*mean_profitBC)
+    format_A_BC<-sum(p_adjustBC[51:100]) # percentage CS
 
     gA<-cbind(gA,p_adjustA)
     gBC<-cbind(gBC,p_adjustBC)
+    profitA<-cbind(profitA,profit_A)
+    profitBC<-cbind(profitBC,profit_BC)
+    formatA_BC<-cbind(formatA_BC,format_A_BC)
 
     distA <-DiscreteDistribution(supp = pricesA, prob = c(p_adjustA))
     distBC <-DiscreteDistribution(supp = pricesBC, prob = c(p_adjustBC))
@@ -119,8 +125,42 @@ for(mu in unique(vmu)){
   assign(nam,cbind(gA))
   nam <- paste("resultsBC_", mu,"_", lambda, sep = "")
   assign(nam,cbind(gBC))
+  nam <- paste("profitA_", mu,"_", lambda, sep = "")
+  assign(nam,cbind(profitA))
+  nam <- paste("profitBC_", mu,"_", lambda, sep = "")
+  assign(nam,cbind(profitBC))
+  nam <- paste("formatA_BC_", mu,"_", lambda, sep = "")
+  assign(nam,cbind(formatA_BC))
+  
   }
 }
+
+# make table of results
+profits_name<-NA
+tab_profitsA<-NA
+tab_profitsBC<-NA
+tab_formats<-NA
+
+for(mu in unique(vmu)){
+  for(lambda in unique(vlambda)){
+    profits_A<-paste("profitA",mu,lambda,sep="_")
+    profits_BC<-paste("profitBC",mu,lambda,sep="_")
+    format_name<-paste("formatA_BC",mu,lambda,sep="_")
+    vecA<-get(profits_A)
+    vecBC<-get(profits_BC)
+    vec_f<-get(format_name)
+    profitsA<-mean(vecA[,95:100])
+    profitsBC<-mean(vecBC[,95:100])
+    formats<-mean(vec_f[,95:100])
+    tab_profitsA<-rbind(tab_profitsA,c(mu,lambda,profitsA))
+    tab_profitsBC<-rbind(tab_profitsBC,c(mu,lambda,profitsBC))
+    tab_formats<-rbind(tab_formats,c(mu,lambda,formats))
+  }}
+
+tab_profitsA
+tab_profitsBC    
+tab_formats
+
 
 # giving out results, by stage in evolution of probabilities of each prices
 
